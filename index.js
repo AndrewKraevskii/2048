f19119b555f0e89cb7c067a29d90df34cca229f1
Andrew <andrew.kraevskii@gmail.com>
2021-09-02 23:29:19 +0300

    fix bug number added when no movement and add more style

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const field_size_px = 700;
 const field_size_cells = 4;
 const cell_margin = 7;
-const font_size = 100;
 const start_numbers_count = 2;
+const font_size = 2;
 
-background_color = {
-    '2': '#eee4da',
-    '4': '#eee1c9',
-    '8': '#f3b27a',
-    '16': '#f69664',
-    '32': '#f77c5f',
-    '64': '#f75f3b',
-    '128': '#edd073',
-    '256': '#edcc62',
-    '512': '#edc950',
-    '1024': '#edc53f',
-    '2048': '#edc22e'
+background = {
+    '2': { color: '#eee4da', font_size: 55 },
+    '4': { color: '#eee1c9', font_size: 55 },
+    '8': { color: '#f3b27a', font_size: 55 },
+    '16': { color: '#f69664', font_size: 55 },
+    '32': { color: '#f77c5f', font_size: 55 },
+    '64': { color: '#f75f3b', font_size: 55 },
+    '128': { color: '#edd073', font_size: 45 },
+    '256': { color: '#edcc62', font_size: 45 },
+    '512': { color: '#edc950', font_size: 45 },
+    '1024': { color: '#edc53f', font_size: 35 },
+    '2048': { color: '#edc22e', font_size: 35 },
+    'super': { color: '#3c3a33', font_size: 30 },
 }
 
 let field = new Array(field_size_cells)
 for (let i = 0; i < field.length; ++i) {
     field[i] = new Array(field_size_cells)
 }
+
 
 /**
  * Draws a rounded rectangle using the current state of the canvas.
@@ -83,17 +85,17 @@ function draw() {
     var ctx = canvas.getContext('2d');
 
     let box_size = field_size_px / field_size_cells;
-    
+
     ctx.fillStyle = '#bbada0';
     ctx.fillRect(0, 0, field_size_px, field_size_px);
     for (let i = 0; i < field_size_cells; ++i) {
         for (let j = 0; j < field_size_cells; ++j) {
             if (field[i][j]) {
-                ctx.fillStyle = background_color[field[i][j]];
+                ctx.fillStyle = background[field[i][j]]?.color || background['super']?.color;
                 roundRect(ctx, box_size * j + cell_margin, box_size * i + cell_margin,
                     box_size - 2 * cell_margin, box_size - 2 * cell_margin,
                     10, true, false)
-                ctx.font = `${font_size}px sans`;
+                ctx.font = `${(background[field[i][j]]?.font_size || background['super'].font_size) * font_size}px sans`;
                 ctx.textAlign = 'center';
                 ctx.textBaseline = 'middle';
                 ctx.fillStyle = 'black';
@@ -108,14 +110,6 @@ function draw() {
             }
         }
     }
-    // for (let i = 0; i < field_size_cells; ++i) {
-    //     for (let j = 0; j < field_size_cells; ++j) {
-    //         ctx.fillStyle = 'black';
-    //         roundRect(ctx, box_size * j + cell_margin, box_size * i + cell_margin,
-    //             box_size - 2 * cell_margin, box_size - 2 * cell_margin,
-    //             10, false, true)
-    //     }
-    // }
 }
 
 function addNumber() {
@@ -131,6 +125,7 @@ function addNumber() {
 
 
 function moveLeft() {
+    let is_any_tile_move = false;
     for (let i = 0; i < field_size_cells; ++i) {
         for (let j = 1; j < field_size_cells; ++j) {
             if (field[i][j] == undefined) {
@@ -141,16 +136,19 @@ function moveLeft() {
                 if (cj < 0) {
                     field[i][cj + 1] = field[i][j];
                     field[i][j] = undefined;
+                    is_any_tile_move = true;
                     break;
                 }
                 if (field[i][cj] != undefined) {
                     if (cj + 1 != j && field[i][cj] != field[i][j]) {
                         field[i][cj + 1] = field[i][j];
                         field[i][j] = undefined;
+                        is_any_tile_move = true;
                     }
                     else if (field[i][cj] == field[i][j]) {
                         field[i][cj] = '' + 2 * (+field[i][j]);
                         field[i][j] = undefined;
+                        is_any_tile_move = true;
                     }
                     break;
                 }
@@ -158,9 +156,11 @@ function moveLeft() {
             }
         }
     }
+    return is_any_tile_move;
 }
 
 function moveUp() {
+    let is_any_tile_move = false;
     for (let i = 1; i < field_size_cells; ++i) {
         for (let j = 0; j < field_size_cells; ++j) {
             if (field[i][j] == undefined) {
@@ -171,16 +171,19 @@ function moveUp() {
                 if (ci < 0) {
                     field[ci + 1][j] = field[i][j];
                     field[i][j] = undefined;
+                    is_any_tile_move = true;
                     break;
                 }
                 if (field[ci][j] != undefined) {
                     if (ci + 1 != i && field[ci][j] != field[i][j]) {
                         field[ci + 1][j] = field[i][j];
                         field[i][j] = undefined;
+                        is_any_tile_move = true;
                     }
                     else if (field[ci][j] == field[i][j]) {
                         field[ci][j] = '' + 2 * (+field[i][j]);
                         field[i][j] = undefined;
+                        is_any_tile_move = true;
                     }
                     break;
                 }
@@ -188,9 +191,11 @@ function moveUp() {
             }
         }
     }
+    return is_any_tile_move;
 }
 
 function moveRight() {
+    let is_any_tile_move = false;
     for (let i = 0; i < field_size_cells; ++i) {
         for (let j = field_size_cells - 2; j >= 0; --j) {
             if (field[i][j] == undefined) {
@@ -201,16 +206,19 @@ function moveRight() {
                 if (cj >= field_size_cells) {
                     field[i][cj - 1] = field[i][j];
                     field[i][j] = undefined;
+                    is_any_tile_move = true;
                     break;
                 }
                 if (field[i][cj] != undefined) {
                     if (cj - 1 != j && field[i][cj] != field[i][j]) {
                         field[i][cj - 1] = field[i][j];
                         field[i][j] = undefined;
+                        is_any_tile_move = false;
                     }
                     else if (field[i][cj] == field[i][j]) {
                         field[i][cj] = '' + 2 * (+field[i][j]);
                         field[i][j] = undefined;
+                        is_any_tile_move = true;
                     }
                     break;
                 }
@@ -218,9 +226,11 @@ function moveRight() {
             }
         }
     }
+    return is_any_tile_move;
 }
 
 function moveDown() {
+    let is_any_tile_move = false;
     for (let i = field_size_cells - 2; i >= 0; --i) {
         for (let j = 0; j < field_size_cells; ++j) {
             if (field[i][j] == undefined) {
@@ -231,16 +241,19 @@ function moveDown() {
                 if (ci >= field_size_cells) {
                     field[ci - 1][j] = field[i][j];
                     field[i][j] = undefined;
+                    is_any_tile_move = true;
                     break;
                 }
                 if (field[ci][j] != undefined) {
                     if (ci - 1 != i && field[ci][j] != field[i][j]) {
                         field[ci - 1][j] = field[i][j];
                         field[i][j] = undefined;
+                        is_any_tile_move = true;
                     }
                     else if (field[ci][j] == field[i][j]) {
                         field[ci][j] = '' + 2 * (+field[i][j]);
                         field[i][j] = undefined;
+                        is_any_tile_move = true;
                     }
                     break;
                 }
@@ -248,28 +261,32 @@ function moveDown() {
             }
         }
     }
+    return is_any_tile_move;
 }
 
 document.addEventListener('keydown', (event) => {
+    let need_new_number = false;
     switch (event.code) {
         case 'ArrowLeft':
-            moveLeft();
+            need_new_number = moveLeft();
             break;
         case 'ArrowUp':
-            moveUp();
+            need_new_number = moveUp();
             break;
         case 'ArrowRight':
-            moveRight();
+            need_new_number = moveRight();
             break;
         case 'ArrowDown':
-            moveDown();
+            need_new_number = moveDown();
             break;
         default:
             return;
 
     }
-    addNumber();
-    draw();
+    if (need_new_number) {
+        addNumber();
+        draw();
+    }
 })
 
 
